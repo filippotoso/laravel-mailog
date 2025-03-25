48371485b03cae1f97bea77e65123987f6cea54a
Filippo Toso <filippo.toso@creativepark.it>
2025-03-25 16:18:57 +0100

    Fixes indexes length

--- database/migrations/create_mailog_tables.php
+++ database/migrations/create_mailog_tables.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,8 +19,8 @@ return new class extends Migration
             $table->text('path')->nullable();
             $table->timestamps();
 
-            $table->index(['date', 'subject']);
-            $table->index(['subject']);
+            $table->index(['date', DB::raw('subject(256)')], 'date_subject_idx');
+            $table->index([DB::raw('subject(256)')], 'subject_idx');
         });
 
         Schema::create(Config::get('mailog.tables.message_addresses'), function (Blueprint $table) {
@@ -34,7 +35,7 @@ return new class extends Migration
             $table->index(['message_id', 'type'], 'type_idx');
             $table->index(['message_id', 'address', 'type'], 'address_idx');
             $table->index(['message_id', 'domain', 'type'], 'domain_idx');
-            $table->index(['message_id', 'name', 'type'], 'name_idx');
+            $table->index(['message_id', DB::raw('name(256)'), 'type'], 'name_idx');
         });
 
         Schema::create(Config::get('mailog.tables.message_attachments'), function (Blueprint $table) {
